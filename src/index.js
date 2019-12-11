aedae5fb7adab2bd2dddde66ff6a378c0fd7881b
Gallagher, Kurt <kgallagher@cvent.com>
2019-12-11 13:52:29 -0500

    ignore node_modules

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-const fetch = require('node-fetch');
+const realFetch = require('node-fetch');
 const SourceMapConsumer = require('source-map').SourceMapConsumer;
 
 const cache = {};
+const fetch = url => {
+    if (cache[url]) {
+        console.log('cache hit');
+        return Promise.resolve(cache[url]);
+    }
 
-const resolveMap = sourceFileUrl => cache[sourceFileUrl] || fetch(sourceFileUrl)
-    .then(response => response.text())
-    .then(text => /\r?\n\/\/# sourceMappingURL=(.*)$/.exec(text)[1])
+    console.log('cache miss');
+    return realFetch(url)
+        .then(response => response.text())
+        .then(body => {
+            cache[url] = body;
+            return body
+        });
+};
+
+const resolveMap = sourceFileUrl => fetch(sourceFileUrl)
+    .then(body => /\r?\n\/\/# sourceMappingURL=(.*)$/.exec(body)[1])
     .then(sourceFile => sourceFileUrl.replace(/[^\/]+$/, sourceFile))
     .then(fetch)
-    .then(response => response.text())
-    .then(body => {
-        const response = {
-            sourceFileUrl,
-            body
-        };
-        cache[sourceFileUrl] = response;
-        return response;
-    });
+    .then(body => ({
+        sourceFileUrl,
+        body
+    }));
 
 const parseStackTrace = requestBody => {
     const stackTraceLines = requestBody.split(/\r?\n/);
@@ -84,38 +92,44 @@ const toApiGatewayResponse = (object, code) => ({
     body: JSON.stringify(object) 
 });
 
+const apiGatewayCallback = callback => (error, success) => {
+    if (error !== null) { 
+        callback(toApiGatewayResponse(error, 500));
+        return;
+    }
+
+    callback(null, toApiGatewayResponse(success, 200));
+};
+
 exports.handler = (event, context, callback) => {
-    const apiGatewayCallback = (error, success) => {
-        if (error !== null) { 
-            callback(toApiGatewayResponse(error, 500));
-            return;
-        }
-
-        callback(null, toApiGatewayResponse(success, 200));
-    };
-
     try {
-        const { stackTrace } = JSON.parse(event.body);
-    
+        const { stackTrace } = JSON.parse(event.body);    
         const parsedStackTrace = parseStackTrace(stackTrace);
     
         parseOriginalPositions(parsedStackTrace)
-            .then(result => apiGatewayCallback(null, {
+            .then(result => apiGatewayCallback(callback)(null, {
                 message: parsedStackTrace.message,
                 stackTrace: result
             }));
     } catch (exception) {
-        apiGatewayCallback(exception, null);
+        apiGatewayCallback(callback)(exception, null);
     }
 };
 
-/*
+
 const body = {
     "stackTrace": "TypeError: Cannot set property 'lastIndex' of undefined\nat r (https://staging.cvent-assets.com/event-guestside-site/assets/41.prod._v4.bdb4b078d70fcbbe35ee819a656d0f44.js:1:254385)\nat t.render (https://staging.cvent-assets.com/event-guestside-site/assets/56.prod._v4.17a5c6994fa1f90b1f9e009b5c3d2409.js:1:1732)\nat c._renderValidatedComponentWithoutOwnerOrContext (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:245414)\nat c._renderValidatedComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:245541)\nat c.performInitialMount (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:241382)\nat c.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:240428)\nat Object.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:294148)\nat c.performInitialMount (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:241542)\nat c.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:240428)\nat Object.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:294148)"
 };
+const request = { body: JSON.stringify(body) };
+const doit = (times, error, success) => {
+    if (error) { console.log(error); }
+    if (success) { console.log(success); }
 
-exports.handler({ body: JSON.stringify(body) }, null, (error, success) => console.log(success));
-*/
+    if (times === 0) { return; }
+    exports.handler(request, null, (e, s) => doit(times - 1, e, s));
+}
+
+doit(2);
 
 // console.log('TypeError: Cannot set property \'lastIndex\' of undefined\nat r (https://staging.cvent-assets.com/event-guestside-site/assets/41.prod._v4.bdb4b078d70fcbbe35ee819a656d0f44.js:1:254385)\nat t.render (https://staging.cvent-assets.com/event-guestside-site/assets/56.prod._v4.17a5c6994fa1f90b1f9e009b5c3d2409.js:1:1732)\nat c._renderValidatedComponentWithoutOwnerOrContext (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:245414)\nat c._renderValidatedComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:245541)\nat c.performInitialMount (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:241382)\nat c.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:240428)\nat Object.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:294148)\nat c.performInitialMount (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:241542)\nat c.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:240428)\nat Object.mountComponent (https://staging.cvent-assets.com/event-guestside-site/assets/vendor.prod._v4.ad6bc0015df2196c861395be5776ba0a.js:1:294148)');
 
